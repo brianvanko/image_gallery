2f6737ad430bfb7fdb1ad5f410f11e848b75a50b
Brian Vanko <brianvanko@gmail.com>
2013-12-17 15:53:25 -0800

    added amazon s3 for paperclip uploads to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,15 @@ ImgGallery::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
    # required for heroku
    # set to actual heroku url
   config.action_mailer.default_url_options = { :host => 'http://pinsy.herokuapp.com/' }
