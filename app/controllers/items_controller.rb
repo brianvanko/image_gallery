aaeeb8e5f20b532ab761b831adf8458336d09cef
Brian Vanko <brianvanko@gmail.com>
2013-12-18 16:05:40 -0800

    load in reverse order

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def show
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
   end
 
   def edit_index
-    @items = Item.all
+     @items = Item.all.order("created_at DESC")
   end
 
   def create
